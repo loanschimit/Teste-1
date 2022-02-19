860858c04dee1c7587dfc5d0845b18d04db42fe9
Loan Schimit <loan.v.schimit1@gmail.com>
2022-02-19 16:13:06 -0300

    otimizado para producao

--- js/script.js
+++ js/script.js
@@ -1,35 +1,74 @@
-const params = new URLSearchParams(location.search);
-
-params.forEach((item) => {
-  const element = document.querySelector(`[type="radio"][value="${item}"]`);
-  if (element) element.checked = true;
-});
-
-const dts = document.querySelectorAll("dt");
-
-dts.forEach((dt) => {
-  dt.addEventListener("click", () => {
-    dt.parentElement.classList.toggle("ativa");
-  });
-});
-
+// Ativar links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
-links.forEach((link) => {
-  if (location.href.includes(link.href)) {
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
     link.classList.add("ativo");
   }
-});
+}
 
-const galeria = document.querySelectorAll(".bicicleta-imagens img");
-const galeriaContainer = document.querySelector(".bicicleta-imagens");
+links.forEach(ativarLink);
 
-galeria.forEach((img) => {
-  img.addEventListener("click", () => {
-    if (matchMedia("(min-width: 1000px)").matches) {
-      galeriaContainer.prepend(img);
-    }
-  });
-});
+// Ativar itens do Orçamento
 
-// if (window.SimpleAnime) new SimpleAnime();
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+// console.log(parametros);
+
+// perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", "true");
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// // // /// Galeria de Imagens
+
+const galeria = document.querySelectorAll(".bicicleta-imagem img");
+const galeriaContainer = document.querySelector(".bicicleta-imagem");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+galeria.forEach(eventosGaleria);
+
+// Animação
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
+
